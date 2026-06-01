a67da40c0f01dda4f3cb86c6469000c13f45acef
Manas <manaskoti00@gmail.com>
2026-06-01 14:19:36 +0530

    remove old p5.js files and add new vector implementation with addition and subtraction methods

---
+++ vectors (p5.js)/2. add, sub/script.js
@@ -0,0 +1,35 @@
+class Vec2 {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  add(v) {
+    this.x += v.x;
+    this.y += v.y;
+  }
+  sub(v) {
+    this.x -= v.x;
+    this.y -= v.y;
+  }
+  mag() {
+    return Math.sqrt((this.x * this.x) + (this.y * this.y));
+  }
+  draw(color) {
+    fill(color);
+    stroke("white")
+    circle(this.x, this.y ,10);
+  }
+}
+
+let pos = new Vec2(10, 20);
+let vel = new Vec2(2, 1);
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background("black");
+    pos.draw("red");
+    pos.add(vel);
+}
