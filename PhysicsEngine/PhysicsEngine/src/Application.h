0a2d20c7c49460660c76d58c04ddab072b46104a
Manas <manaskoti00@gmail.com>
2026-06-17 18:05:49 +0530

    chain spring

--- PhysicsEngine/PhysicsEngine/src/Application.h
+++ PhysicsEngine/PhysicsEngine/src/Application.h
@@ -6,28 +6,27 @@
 #include <vector>
 
 class Application {
-    private:
-        bool running = false;
-		std::vector<Particle*> particles;
-        Vec2 pushForce = Vec2(0,0);
-        Vec2 mouseCursor = Vec2(0, 0);
-        bool leftMouseButtonDown = false;
+private:
+    bool running = false;
+    std::vector<Particle*> particles;
+    Vec2 pushForce = Vec2(0, 0);
+    Vec2 mouseCursor = Vec2(0, 0);
+    bool leftMouseButtonDown = false;
 
-		Vec2 anchor;
-        float k = 40;
-		float restLength = 200;
+    Vec2 anchor;
+    float k = 300;
+    float restLength = 15;
+    const int NUM_PARTICLES = 15;
 
-        SDL_Rect liquid;
-
-    public:
-        Application() = default;
-        ~Application() = default;
-        bool IsRunning();
-        void Setup();
-        void Input();
-        void Update();
-        void Render();
-        void Destroy();
+public:
+    Application() = default;
+    ~Application() = default;
+    bool IsRunning();
+    void Setup();
+    void Input();
+    void Update();
+    void Render();
+    void Destroy();
 };
 
 #endif
